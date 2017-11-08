9d280e29acab9ee5424f8b83dcc22f646f8fc6b5
Tetens <clte@sdc.dk>
2017-11-08 13:19:44 +0100

    SQL server used

---
+++ WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            /*
+            if(VariableName.DatabaseExists()) {
+                MessageBox.Show("Data is in");
+            }
+            */
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+
+        ProductContext productContext=new ProductContext();
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            Category category = new Category();
+            category.CategoryId = 6037;
+            category.Name="myCat";
+
+            productContext.Categories.Add(category);
+
+            Product product = new Product();
+            product.Category = category;
+            product.CategoryId = category.CategoryId;
+            product.Name = "Duck";
+            product.ProductId = 6042;
+
+            productContext.Products.Add(product);
+
+            productContext.SaveChanges();
+        }
+    }
+}
---
+++ WpfApplication4/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+
+namespace WpfApplication4 {
+    public class Product {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+
+    public class Category {
+        public Category() {
+            this.Products = new ObservableCollection<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ObservableCollection<Product> Products { get; private set; }
+    }
+
+
+    public class ProductContext : DbContext {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
